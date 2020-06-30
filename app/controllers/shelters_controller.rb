4dd2ce729ea2942348a4f92d2bb1508da14dcaaf
Gaby Mendez <gmendez90@gmail.com>
2020-06-30 18:33:54 -0400

    Write shelters#create

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -8,9 +8,19 @@ class SheltersController < ApplicationController
   end
 
   def new
+
   end
 
   def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+    shelter.save
+    redirect_to '/shelters'
   end
 
   def edit
