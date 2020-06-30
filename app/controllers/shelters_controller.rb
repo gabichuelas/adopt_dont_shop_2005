d71d7aa2337c87f8725ff7ff7879eaa3ca6eddf4
Gaby Mendez <gmendez90@gmail.com>
2020-06-30 16:27:01 -0400

    Write structure for rest of SheltersController

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,22 @@ class SheltersController < ApplicationController
   def index
     @shelters = Shelter.all
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end 
 end
