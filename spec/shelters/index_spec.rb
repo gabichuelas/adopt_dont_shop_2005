379e21bf2d13c7a9e1d94585515b86eef3e2ac9a
Gaby Mendez <gmendez90@gmail.com>
2020-06-30 14:16:15 -0400

    Rename shelter index spec file & description

---
+++ spec/shelters/index_spec.rb
@@ -0,0 +1,18 @@
+# As a visitor
+# When I visit '/shelters'
+# Then I see the name of each shelter in the system
+
+RSpec.describe "visit /shelters index", type: :feature do
+  it "list of shelter names" do
+    shelter1 = Shelter.create(name: "Shelter 1")
+    shelter2 = Shelter.create(name: "Shelter 2")
+
+    visit '/shelters'
+
+    # expect(page).to have_content(shelter1.title)
+    # expect(page).to have_content(shelter2.title)
+
+    page.has_content?(shelter1.title)
+    page.has_content?(shelter2.title)
+  end
+end
