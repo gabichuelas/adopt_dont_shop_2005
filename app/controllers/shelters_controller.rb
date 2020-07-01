b37325a11ad6f3627051a2ee4db34d4bcab6eed3
Gaby Mendez <gmendez90@gmail.com>
2020-07-01 01:04:24 -0400

    Write #update, edit view

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -12,14 +12,18 @@ class SheltersController < ApplicationController
   end
 
   def create
-    shelter = Shelter.create(shelter_params)
+    Shelter.create!(shelter_params)
     redirect_to "/shelters"
   end
 
   def edit
+    @shelter = Shelter.find(params[:id])
   end
 
   def update
+    @shelter = Shelter.find(params[:id])
+    @shelter.update(shelter_params)
+    redirect_to "/shelters/#{@shelter.id}"
   end
 
   def destroy
---
+++ spec/features/shelters/edit_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe "update existing shelter", type: :feature do
+  it "uses 'update shelter' link to visit /shelters/id/edit, fill out form, and update shelter" do
+
+    shelter1 = Shelter.create!({name: "Shelter 1", address: "1234 NW 10th St.", city: "Gainesville", state: "FL", zip: 32609})
+
+    visit "/shelters/#{shelter1.id}"
+    click_on "Update Shelter"
+    expect(current_path).to eq("/shelters/#{shelter1.id}/edit")
+
+    fill_in('name', with: 'Shelter 1b')
+    click_on('Save')
+    expect(current_path).to eq("/shelters/#{shelter1.id}")
+    page.has_content?('Shelter 1b')
+  end
+end
