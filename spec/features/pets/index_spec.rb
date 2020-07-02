030b8ab0cd683f2e02003f16d16baa6e9560075d
Gaby Mendez <gmendez90@gmail.com>
2020-07-01 22:49:36 -0400

    Write pets#index_spec

---
+++ spec/features/pets/index_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'as a visitor' do
+  describe 'when I visit /pets' do
+    it 'then I see each Pet in the system including image, name, approximate age, sex, name of shelter' do
+
+      shelter = Shelter.create(name: "Braun Farm")
+      pet1 = Pet.create(name: 'Noodle', approx_age: 3, sex: "M", image: "", shelter_id: shelter.id )
+      pet2 = Pet.create(name: 'Marley', approx_age: 12, sex: "F", image: "", shelter_id: shelter.id )
+
+      visit '/pets'
+
+      expect(page).to have_content(pet1.name)
+      expect(page).to have_content(pet1.approx_age)
+      expect(page).to have_content(pet1.sex)
+      expect(page).to have_content(pet1.image)
+      expect(page).to have_content("Braun Farm")
+      expect(page).to have_content(pet2.name)
+    end
+  end
+end
