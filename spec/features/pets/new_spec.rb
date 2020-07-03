ad3268a122dc08f6201e940975fce7dfaa1df6d2
Gaby Mendez <gmendez90@gmail.com>
2020-07-03 14:47:49 -0400

    Write pets#new spec

---
+++ spec/features/pets/new_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe 'as a visitor' do
+  it 'when I visit shelter/pets index, use create pet link to fill in form with pet details and add new pet' do
+
+    shelter = Shelter.create!(name: "Braun Farm")
+
+    visit "/shelters/#{shelter.id}/pets"
+    click_on 'Create Pet'
+    expect(current_path).to eq("/shelters/#{shelter.id}/pets/new")
+
+    name = "Bella"
+    age = 1
+    description = "Young and spunky ACD"
+    image = "https://upload.wikimedia.org/wikipedia/commons/c/cc/ACD-blue-spud.jpg"
+    sex = 'female'
+
+    fill_in :name, with: name
+    fill_in :approx_age, with: age
+    fill_in :description, with: description
+    fill_in :sex, with: sex
+    fill_in :image, with: image
+    # come back to this and create an upload interface
+
+    click_on 'Save Pet'
+    expect(current_path).to eq("/shelters/#{shelter.id}/pets")
+
+    expect(page).to have_content("Name: Bella")
+    expect(page).to have_content("Status: adoptable")
+    expect(page).to have_content("Braun Farm")
+
+  end
+end
