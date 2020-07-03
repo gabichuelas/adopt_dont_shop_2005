92134ff1460a5402ffd6b29985b26bf516438a2a
Gaby Mendez <gmendez90@gmail.com>
2020-07-03 14:28:43 -0400

    Create migration for pets description, create pets#show view

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'as a visitor' do
 
     shelter = Shelter.create(name: "Braun Farm")
 
-    pet1 = Pet.create!(name: 'Noodle', approx_age: 3, sex: "male", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id, status: "adoptable" )
+    pet1 = Pet.create!(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id, status: "adoptable" )
 
     visit "/pets/#{pet1.id}"
 
