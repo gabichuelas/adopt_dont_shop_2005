81b188775ab995c998d3d8714c0da6268eb27508
Gaby Mendez <gmendez90@gmail.com>
2020-07-03 14:51:17 -0400

    Add 'create pet' link to shelter pets index page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/shelters/:shelter_id/pets', to: 'shelters#pets_index'
+  get '/shelters/:shelter_id/pets/new', to: 'pets#new'
+
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
