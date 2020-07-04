81d37626f37b56ae6630138ca91e5c3c6369a37f
Gaby Mendez <gmendez90@gmail.com>
2020-07-04 19:13:03 -0400

    Edit pets index_spec

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'as a visitor' do
 
       visit '/pets'
 
-      expect(page).to have_content("Name: Noodle")
-      expect(page).to have_content("Approximate Age: #{pet1.approx_age}")
-      expect(page).to have_content("Sex: #{pet1.sex}")
+      expect(page).to have_content("Noodle")
+      expect(page).to have_content("#{pet1.approx_age}")
+      expect(page).to have_content("#{pet1.sex}")
 
       expect(page).to have_xpath("//img[@src = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg' and @alt='photo of pet']")
 
