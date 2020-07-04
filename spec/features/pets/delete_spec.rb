e65a261d4900726a434c1310770126867b00e4db
Gaby Mendez <gmendez90@gmail.com>
2020-07-04 11:34:20 -0400

    Write pets delete_spec

---
+++ spec/features/pets/delete_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe "as a visitor", type: :feature do
+  describe 'destroy existing pet' do
+    it 'by using delete pet link on pet show page, redirect to pets index' do
+
+      shelter = Shelter.create(name: "Braun Farm")
+
+      pet1 = Pet.create!(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id, status: "adoptable" )
+
+      visit "/pets"
+      click_on "Delete Pet"
+      expect(current_path).to eq("/pets")
+
+      expect(page).to_not have_content('Noodle')
+
+    end
+  end
+end
