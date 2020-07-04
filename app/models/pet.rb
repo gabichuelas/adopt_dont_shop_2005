efd99ecb6b0902f07828d4c731e4a21d48cf7cac
Gaby Mendez <gmendez90@gmail.com>
2020-07-03 22:10:53 -0400

    Remove validates :status on pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,6 +3,6 @@ class Pet < ApplicationRecord
   validates_presence_of :approx_age
   validates_presence_of :sex
   validates_presence_of :shelter
-  validates_presence_of :status
+  # validates_presence_of :status
   belongs_to :shelter
 end
